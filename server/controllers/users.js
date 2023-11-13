e8dbacfac229b4e036f656c72d4c68f369e8fcab
Jaime-prog <a00571842@tec.mx>
2023-11-13 16:12:46 -0600

    Refactored code to improve performance and readability.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -17,20 +17,33 @@ export const getUserFriends = async (req, res) => {
     const { id } = req.params;
     const user = await User.findById(id);
 
-      const friends = await Promise.all(
-          user.friends.map((id) => User.findById(id))
-      );
-      const formattedFriends = friends.map(
-          ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-              return { _id, firstName, lastName, occupation, location, picturePath};
-          }
-      );
-      res.status(200).json(formattedFriends);
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
   } catch (error) {
-      res.status(404).json({ message: error.message });
+    res.status(404).json({ message: error.message });
   }
 };
 
 /*UPDATE */
 
-export const addRemovedFriend = async (req, res) => {};
+export const addRemovedFriend = async (req, res) => {
+  try {
+    const { id, friendId } = req.params;
+    const user = await User.findById(id);
+    const friend = await User.findById(friendId);
+
+    if (user.friends.includes(friendId)) {
+      user.friends = user.friends.filter((id) => id !== friendId);
+      friend.friends = friend.friends.filter((id) => id !== id);
+    }
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
