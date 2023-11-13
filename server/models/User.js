21616a16212c7ec90a127215d9eafd2ad65e89b3
Jaime-prog <a00571842@tec.mx>
2023-11-13 14:04:36 -0600

    Adds initial configuration & authentication method

---
+++ server/models/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 60,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        min: 2,
+        max: 60,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        max: 60,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+    },
+    picturePath: {
+        type: String,
+        default: "",
+    },
+    friends: {
+        type: Array,
+        default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+}, { timestamps: true });
+
+const User = mongoose.model("User", UserSchema);
+export default User;
