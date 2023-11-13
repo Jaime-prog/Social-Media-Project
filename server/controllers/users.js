38c2592b7d82e69afa5591d2ac47f99c033392f4
Jaime-prog <a00571842@tec.mx>
2023-11-13 16:15:29 -0600

    Fix addRemovedFriend function to properly update user and friend's friends list

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -42,7 +42,23 @@ export const addRemovedFriend = async (req, res) => {
     if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
       friend.friends = friend.friends.filter((id) => id !== id);
+    } else {
+      user.friends.push(friendId);
+      friend.friends.push(id);
     }
+
+    await user.save();
+    await friend.save();
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
