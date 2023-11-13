9ebd0eaa9f34e7db6d440f496202deda3f214cbb
Jaime-prog <a00571842@tec.mx>
2023-11-13 16:17:38 -0600

    Add postRoutes to server index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import usersRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
 import {register} from "./controllers/auth.js";
 
 /*Configurations */
@@ -46,6 +47,7 @@ app.post("/auth/register", upload.single("picture"), register)
 /*ROUTES*/
 app.use("/auth", authRoutes);
 app.use("/users", usersRoutes);
+app.use('/posts', postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
