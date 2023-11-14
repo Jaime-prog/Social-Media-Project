fd2af636e0f582b9459aa1e40eede51f65015cfe
Jaime-prog <a00571842@tec.mx>
2023-11-13 21:12:24 -0600

    Update userRoutes and server configurations

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser, getUserFriends, addRemovedFriend } from "../controllers/users.js";
+import { getUser, getUserFriends, addRemoveFriend } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -7,6 +7,6 @@ const router = express.Router();
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
 /* UPDATE */
-router.patch("/:id/:friendId", verifyToken, addRemovedFriend);
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
