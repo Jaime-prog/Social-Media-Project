0f2e96b98e9dcfdd93629cac350ba8cc2f688c2d
Jaime-prog <a00571842@tec.mx>
2023-11-13 18:55:14 -0600

    Refactor code to use MUI and Redux

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,3 +1,10 @@
+import { useDispatch, useSelector } from "react-redux"
+import { setMode, setLogout } from "../../redux/actions"
+import { useNavigate } from "react-router-dom"
+import FlexBetween from "components/FlexBetween"
+
+
+
 const Navbar = () => {
     return (<div>Navbar</div>)
 }
