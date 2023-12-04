db6b71f3f3e90ba9c730ada3ce37bca658eb1366
Jaime-prog <a00571842@tec.mx>
2023-12-04 16:06:26 -0600

    Update page title and widget border radius

--- client/src/components/WidgetWrapper.jsx
+++ client/src/components/WidgetWrapper.jsx
@@ -5,6 +5,6 @@ import { styled } from "@mui/system";
 const WidgetWrapper = styled(Box)(({ theme }) => ({
     padding: "1.5 rem 1.5 rem 0.75 rem  1.5 rem",
     backgroundColor: theme.palette.background.alt,
-    borderRadius: "0.5rem",
+    borderRadius: "0.75rem",
 }));
 export default WidgetWrapper;
